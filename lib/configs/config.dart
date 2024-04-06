897b94eb942d825fc78dcfe7f40df59437e46c38
rvrajaneesh <rvrajaneesh@gmail.com>
2024-04-06 16:56:00 +0530

    First-commit-StaggeredTile-error

---
+++ lib/configs/config.dart
@@ -0,0 +1,35 @@
+
+
+class Config {
+  
+  final String appName = 'PixelWall';
+  final String hashTag = '#pixelwall';
+  
+  final String packageName = 'wallpaper_app';
+  final String appVersion = '3.1.0';
+
+  final String appIcon = 'assets/images/icon.png';
+  final String splashIcon = 'assets/images/splash.png';
+
+  
+  final String guestAvatar = 'assets/images/guest_avatar.png';
+
+  //Explore screen
+  static const String popularItemsName = 'WallPapers';
+  static const String newItemsName = 'Latest';
+
+  //Links
+  static const String privacyUrl = "httpscom/privacy-policy";
+
+  // -- Onesignal Push Notification
+  final String onesignalAppId = '8c44bae4-195f-43c5-bba8-e901757be479';
+  
+  //-- admob ads --
+  final String admobAppId = 'ca-app-pub-3940256099942544~3347511713';
+  final String admobInterstitialAdId = 'ca-app-pub-3940256099942544/1033173712';
+
+
+  //-- facebook ads--
+  //final String facebookInterstitialAdId = '587840845185406_587842688518555';
+
+}
